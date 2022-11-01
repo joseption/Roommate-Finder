e3ac246dd0fd91435dd09848a726f30ebc256f18
joseption <40270262+joseption@users.noreply.github.com>
2022-11-01 14:47:16 -0400

    nav in process

--- client/App.tsx
+++ client/App.tsx
@@ -1,4 +1,3 @@
-import React, { FC, useEffect, useState } from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import LoginScreen from './screens/login';
@@ -7,8 +6,6 @@ import { useFonts } from 'expo-font';
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { faCheck, faXmark, faMessage, faCaretDown, faUser, faPoll, faHouseFlag, faCheckDouble } from '@fortawesome/free-solid-svg-icons'
 import Navigation from './components/navigation';
-import { Dimensions } from 'react-native';
-import { isMobile } from './service';
 
 library.add(faCheck, faXmark, faMessage, faCaretDown, faUser, faPoll, faHouseFlag, faCheckDouble)
 

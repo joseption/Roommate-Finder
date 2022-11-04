4280b2edaa31f77fb3af470c1c3f2ba9872e61c8
Sleepingpirates <faizsyed.a@gmail.com>
2022-11-04 14:38:09 -0400

    feat(users): added get profile endpoints

--- server/src/api/users/routes.ts
+++ server/src/api/users/routes.ts
@@ -1,7 +1,7 @@
 import express, { Request, Response, NextFunction } from 'express';
 import { isAuthenticated } from '../../middleware';
 import { findUserById } from './services';
-
+import db from '../../utils/db';
 const router = express.Router();
 
 router.get('/profile', isAuthenticated, async (req:Request, res:Response, next:NextFunction) => {
@@ -10,6 +10,25 @@ router.get('/profile', isAuthenticated, async (req:Request, res:Response, next:N
     const user = await findUserById(userId);
     delete user.password;
     res.json(user);
+  } catch (err) {
+    next(err);
+  }
+});
+
+router.get('/profile', isAuthenticated, async (req:Request, res:Response, next:NextFunction) => {
+  try {
+    const { userId } = req.body[0];
+    const user = await findUserById(userId);
+    delete user.password;
+    res.json(user);
+  } catch (err) {
+    next(err);
+  }
+});
+
+router.get('/Allprofiles', isAuthenticated, async (req:Request, res:Response, next:NextFunction) => {
+  try {
+    return db.user.findMany();
   } catch (err) {
     next(err);
   }

2f317aae6932aeeaebf2ecd5731af31e670b0f4d
Sleepingpirates <faizsyed.a@gmail.com>
2022-11-09 11:58:45 -0500

    fix: get all questions for survery and user res

--- server/src/api/survey/routes.ts
+++ server/src/api/survey/routes.ts
@@ -2,20 +2,34 @@ import express, { Request, Response } from "express";
 const router = express.Router()
 import db from '../../utils/db';
 import {isAuthenticated} from '../../middleware';
+import jwt  from "jsonwebtoken";
 
-router.get("/Questions", isAuthenticated, (req: Request, res: Response) => {
-    return res.json(
-        db.question.findMany({
+router.use(isAuthenticated);
+
+router.get("/question", (req: Request, res: Response) => {
+    try {
+        const payload : payload = req.body[0];
+        const Questions = db.question.findMany({
             select: {
                 id: true,
                 question_text: true,
                 question_text_short: true,
-                is_filter: true,
-                response_id: true,
                 response: true,
+                ResponsesOnUsers: {
+                    where: {
+                        userId: payload.userId,
+                    },
+                    select: {
+                        responseId: true,
+                        response: true
+                    }
+            }
             },
         })
-    );
+        res.json(Questions)
+    } catch (error) {
+        res.status(500).json(error);
+    }
 });
 
 export default router;

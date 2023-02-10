aad7bfd75d5ba32b7a5930bdd980f973eea374b9
Frederick Santiago <frsantiago2001@gmail.com>
2023-02-09 22:43:35 -0500

    feat: add notifications routes

---
+++ server/src/api/notifications/routes.ts
@@ -0,0 +1,59 @@
+import express, { Request, Response } from 'express';
+import { isAuthenticated } from '../../middleware';
+import db from '../../utils/db';
+import { blockedChat } from 'api/messages/messagesHelper';
+const router = express.Router();
+
+// router.use(isAuthenticated);
+
+// send a notification
+router.post('/', async (req: Request, res: Response) => {
+  try {
+    const { userId, chatId } = req.body;
+    const userBlockedChat = await blockedChat(userId, chatId);
+    if (!userBlockedChat) {
+      await db.notifications.create({
+        data: {
+          userId: userId as string,
+          chatId: chatId as string,
+        }
+      });
+    }
+    res.status(200);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+// get all notifications for a user
+router.get('/:userId', async (req: Request, res: Response) => {
+  try {
+    const { userId } = req.params;
+    const notifications = await db.notifications.findMany({
+      where: {
+        userId: userId as string,
+      }
+    });
+    res.status(200).json(notifications);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+// delete all notifications for a given chat
+router.delete('/', async (req: Request, res: Response) => {
+  try {
+    const { userId, chatId } = req.body;
+    const deleted = await db.notifications.deleteMany({
+      where: {
+        userId: userId as string,
+        chatId: chatId as string,
+      }
+    });
+    res.status(200).json(deleted)
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+export default router;

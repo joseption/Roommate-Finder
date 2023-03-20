f7e8e3ff65c3fa1775002c932ce4846930d1cc9f
Aaronel <60762388+Aaronel10@users.noreply.github.com>
2023-03-20 19:15:20 -0400

    fix: myListing eslint error

--- web/src/pages/mylistings.tsx
+++ web/src/pages/mylistings.tsx
@@ -1,36 +1,11 @@
-import { useQuery } from "@tanstack/react-query";
-import React, { Fragment, useRef, useState } from "react";
+import React from "react";
 
-import ListingContent from "../components/listingsContent";
-import NoListingsModal from "../components/noListingsModal";
-import { GetCurrentUserInfo, GetUserListings } from "../request/fetch";
+import ListingContent from "../components/Listings/myListingsContent";
+
 export default function Mylistings() {
-  const { data: userData, isLoading: userLoading } = useQuery(["UserInfo"], {
-    queryFn: () => GetCurrentUserInfo(),
-    onSuccess: (data) => {
-      console.log(data);
-    },
-    onError: (err) => {
-      console.log(err);
-    },
-  });
-  const { data: myListings, isLoading: listingsLoading } = useQuery(
-    ["UserListings", userData ? userData.id : ""],
-    () => GetUserListings(userData ? userData.id : "")
-  );
   return (
     <div className="">
-      {listingsLoading || !myListings ? (
-        <div>Loading...</div>
-      ) : (
-        <div className="flex">
-          {myListings.length > 0 ? (
-            <ListingContent data={myListings} />
-          ) : (
-            <NoListingsModal />
-          )}
-        </div>
-      )}
+      <ListingContent />
     </div>
   );
 }

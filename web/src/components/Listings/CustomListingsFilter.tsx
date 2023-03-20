572d16fefb05eb15d41c3fac44a3ba9bdad70541
Aaronel <60762388+Aaronel10@users.noreply.github.com>
2023-03-20 04:45:21 -0400

    fix: redo filtering styling

---
+++ web/src/components/Listings/CustomListingsFilter.tsx
@@ -0,0 +1,193 @@
+import { FunnelIcon } from "@heroicons/react/24/outline";
+import { debounce } from "lodash";
+import { useRouter } from "next/router";
+import React, { ChangeEvent, useCallback, useState } from "react";
+
+import Button from "../Inputs/Button";
+import CustomPopover from "../Inputs/CustomPopover";
+
+interface props {
+  price: number;
+  setPrice: (value: number) => void;
+  numberRooms: string;
+  setNumberRooms: (value: string) => void;
+  numberBathrooms: string;
+  setNumberBathrooms: (value: string) => void;
+  petsAllowed: string;
+  setPetsAllowed: (value: string) => void;
+  housingType: string;
+  setHousingType: (value: string) => void;
+  distanceToUcf: number;
+  setDistanceToUcf: (value: number) => void;
+}
+
+export default function CustomListingFilterPopover({
+  price,
+  setPrice,
+  numberRooms,
+  setNumberRooms,
+  numberBathrooms,
+  setNumberBathrooms,
+  petsAllowed,
+  setPetsAllowed,
+  housingType,
+  setHousingType,
+  distanceToUcf,
+  setDistanceToUcf,
+}: props) {
+  const clearFilters = () => {
+    setPrice(100000);
+    setHousingType("all");
+    setNumberRooms("all");
+    setNumberBathrooms("all");
+    setPetsAllowed("all");
+    setDistanceToUcf(100000);
+  };
+
+  const debouncedPrice = React.useRef(
+    debounce((e: React.ChangeEvent<HTMLInputElement>) => {
+      setPrice(Number(e.target.value));
+    }, 600)
+  ).current;
+
+  const handlePriceChange = async (e: React.ChangeEvent<HTMLInputElement>) => {
+    debouncedPrice(e);
+  };
+  const handleHousingTypeChange = (event: ChangeEvent<HTMLSelectElement>) => {
+    setHousingType(event.target.value);
+  };
+
+  const handleDistanceToCampusChange = (
+    event: ChangeEvent<HTMLSelectElement>
+  ) => {
+    setDistanceToUcf(Number(event.target.value));
+  };
+
+  const handleRoomsChange = (event: ChangeEvent<HTMLSelectElement>) => {
+    setNumberRooms(event.target.value);
+  };
+
+  const handleBathroomsChange = (event: ChangeEvent<HTMLSelectElement>) => {
+    setNumberBathrooms(event.target.value);
+  };
+
+  const handlePetPreferenceChange = (event: ChangeEvent<HTMLSelectElement>) => {
+    setPetsAllowed(event.target.value);
+  };
+
+  return (
+    <div>
+      <CustomPopover
+        popoverPlacement={"bottom-end"}
+        button={
+          <Button>
+            Filters
+            <FunnelIcon className={"ml-2 h-6 w-6"} />
+          </Button>
+        }
+      >
+        {(open) => (
+          <div className="space-y-4 rounded p-4">
+            <div className="flex items-center">
+              <label htmlFor="price" className="block">
+                Under $:
+              </label>
+              <input
+                id="price"
+                type="number"
+                className="text-primary focus:ring-primary rounded"
+                // eslint-disable-next-line @typescript-eslint/no-misused-promises
+                onChange={handlePriceChange}
+              />
+            </div>
+            <div>
+              <label htmlFor="housing_type" className="block">
+                Type of home:
+              </label>
+              <select
+                id="htype"
+                className="focus:border-primary focus:ring-primary mt-1 block w-full rounded border border-gray-300 bg-white text-black shadow-sm"
+                value={housingType}
+                onChange={handleHousingTypeChange}
+              >
+                <option value="">All</option>
+                <option value="Apartment">Apartment</option>
+                <option value="House">House</option>
+                <option value="Condo">Condo</option>
+              </select>
+            </div>
+            <div>
+              <label htmlFor="rooms" className="block">
+                Number of rooms:
+              </label>
+              <select
+                id="rooms"
+                className="focus:border-primary focus:ring-primary mt-1 block w-full rounded border border-gray-300 bg-white text-black shadow-sm"
+                value={numberRooms}
+                onChange={handleRoomsChange}
+              >
+                <option value="">All</option>
+                <option value={1}>1</option>
+                <option value={2}>2</option>
+                <option value={3}>3</option>
+                <option value={10}>4+</option>
+              </select>
+            </div>
+            <div>
+              <label htmlFor="bathroomsPref" className="block">
+                Number of bathrooms:
+              </label>
+              <select
+                id="bathrooms"
+                className="focus:border-primary focus:ring-primary mt-1 block w-full rounded border border-gray-300 bg-white text-black shadow-sm"
+                value={numberBathrooms}
+                onChange={handleBathroomsChange}
+              >
+                <option value="">All</option>
+                <option value={1}>1</option>
+                <option value={2}>2</option>
+                <option value={3}>3</option>
+                <option value={10}>4+</option>
+              </select>
+            </div>
+            <div>
+              <label htmlFor="distanceFromCampus" className="block">
+                Max distance to campus:
+              </label>
+              <select
+                id="distanceFromCampus"
+                className="focus:border-primary focus:ring-primary mt-1 block w-full rounded border border-gray-300 bg-white text-black shadow-sm"
+                value={distanceToUcf}
+                onChange={handleDistanceToCampusChange}
+              >
+                <option value="">All</option>
+                <option value={5}>5 miles</option>
+                <option value={8}>8 miles</option>
+                <option value={10}>10 miles</option>
+                <option value={15}>15 miles</option>
+              </select>
+            </div>
+            <div>
+              <label htmlFor="smokePreference" className="block">
+                Pet preference:
+              </label>
+              <select
+                id="petPreference"
+                className="focus:border-primary focus:ring-primary mt-1 block w-full rounded border border-gray-300 bg-white text-black shadow-sm"
+                value={petsAllowed}
+                onChange={handlePetPreferenceChange}
+              >
+                <option value="">All</option>
+                <option value="yes">Allowed</option>
+                <option value="no">Not Allowed</option>
+              </select>
+            </div>
+            <Button onClick={clearFilters} className="w-full">
+              Clear Filters
+            </Button>
+          </div>
+        )}
+      </CustomPopover>
+    </div>
+  );
+}
--- web/src/pages/listings.tsx
+++ web/src/pages/listings.tsx
@@ -1,104 +1,62 @@
 import { useQuery } from "@tanstack/react-query";
+import { motion } from "framer-motion";
 import { debounce } from "lodash";
 import { useRouter } from "next/router";
 import React, { useCallback, useState } from "react";
 
+import CustomListingFilterPopover from "../components/Listings/CustomListingsFilter";
 import ListingContent from "../components/listingsContent";
 import Sidebar from "../components/sidebar";
 import { GetListings } from "../request/fetch";
+import { transitionVariants } from "../styles/motion-definitions";
 import { ListingRequest } from "../types/listings.types";
 
 export default function Listings() {
-  const router = useRouter();
   const [price, setPrice] = useState<number>(100000);
-  const [housingType, setHousingType] = useState<string | undefined>();
-  const [bedrooms, setBedrooms] = useState<number | undefined>();
-  const [bathrooms, setBathrooms] = useState<number>();
-  const [petsAllowed, setPetsAllowed] = useState<boolean | null | undefined>();
-  const [distanceToUCF, setDistanceToUCF] = useState<number>();
+  const [housingType, setHousingType] = useState<string>("all");
+  const [bedrooms, setBedrooms] = useState<string>("all");
+  const [bathrooms, setBathrooms] = useState<string>("all");
+  const [petsAllowed, setPetsAllowed] = useState<string>("all");
+  const [distanceToUCF, setDistanceToUCF] = useState<number>(100000);
 
-  // const debouncedPrice = React.useRef(
-  //   debounce((e: React.ChangeEvent<HTMLInputElement>) => {
-  //     setPrice(Number(e.target.value));
-  //   }, 500)
-  // ).current;
-
-  // const handlePriceChange = async (e: React.ChangeEvent<HTMLInputElement>) => {
-  //   debouncedPrice(e);
-  // await router.push({
-  //   query: { ...router.query, p: Number(e.target.value) },
-  // });
-  // };
-  const handlePriceChange = useCallback(
-    (p: number) => {
-      setPrice(p);
-    },
-    [setPrice]
-  );
-
-  const handleHousingTypeChange = useCallback(
-    (h: string) => {
-      setHousingType(h);
-    },
-    [setHousingType]
-  );
-  const handleBedroomsChange = useCallback(
-    (b: number) => {
-      setBedrooms(b);
-    },
-    [setBedrooms]
-  );
-  const handleBathroomsChange = useCallback(
-    (b: number) => {
-      setBathrooms(b);
-    },
-    [setBathrooms]
-  );
-  const handlePetsAllowedChange = useCallback(
-    async (pA: boolean) => {
-      setPetsAllowed(pA);
-      await router.push({ query: { ...router.query, pA: pA } });
-    },
-    [router]
-  );
-  const handleDistanceToUCFChange = useCallback(
-    async (d: number) => {
-      setDistanceToUCF(d);
-      await router.push({ query: { ...router.query, dtu: d } });
-    },
-    [router]
-  );
-
-  const listingReq: ListingRequest = {
-    price: price,
-    housing_type: housingType,
-    rooms: bedrooms,
-    bathrooms: bathrooms,
-    petsAllowed: petsAllowed,
-    distanceToUcf: distanceToUCF,
-  };
-
-  const { data, isLoading } = useQuery(["listings", listingReq], () =>
-    GetListings(listingReq)
-  );
+  const { data, isLoading } = useQuery(["listings"], () => GetListings());
   return (
     <div className="">
       {isLoading || !data ? (
         <div>Loading...</div>
       ) : (
-        <div className="flex">
-          <Sidebar
-            handlePriceChange={handlePriceChange}
-            handleHousingTypeChange={handleHousingTypeChange}
-            handleBedroomsChange={handleBedroomsChange}
-            handleBathroomsChange={handleBathroomsChange}
-            // eslint-disable-next-line @typescript-eslint/no-misused-promises
-            handlePetsAllowedChange={handlePetsAllowedChange}
-            // eslint-disable-next-line @typescript-eslint/no-misused-promises
-            handleDistanceToUCFChange={handleDistanceToUCFChange}
-          />
-          <ListingContent data={data} />
-        </div>
+        <>
+          <motion.main
+            className={"min-h-screen bg-white"}
+            initial={"fadeOut"}
+            animate={"fadeIn"}
+            exit={"fadeOut"}
+            custom={0.4}
+            variants={transitionVariants}
+          >
+            <div
+              className={
+                "relative mx-auto flex h-full  max-w-7xl items-center justify-end bg-white pr-8 pt-5"
+              }
+            >
+              <CustomListingFilterPopover
+                price={price}
+                setPrice={setPrice}
+                numberRooms={bedrooms}
+                setNumberRooms={setBedrooms}
+                numberBathrooms={bathrooms}
+                setNumberBathrooms={setBathrooms}
+                petsAllowed={petsAllowed}
+                setPetsAllowed={setPetsAllowed}
+                housingType={housingType}
+                setHousingType={setHousingType}
+                distanceToUcf={distanceToUCF}
+                setDistanceToUcf={setDistanceToUCF}
+              />
+            </div>
+            <ListingContent data={data} />
+          </motion.main>
+        </>
       )}
     </div>
   );

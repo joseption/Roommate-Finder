a7a3ff59b81e060493b0d7c902fe5e9ca4919772
Aaronel <60762388+Aaronel10@users.noreply.github.com>
2023-03-22 02:09:07 -0400

    feat: error handling for creating listings and adding create listing button

--- web/src/pages/listings.tsx
+++ web/src/pages/listings.tsx
@@ -1,6 +1,8 @@
 import { motion } from "framer-motion";
+import { useRouter } from "next/router";
 import React, { useState } from "react";
 
+import Button from "../components/Inputs/Button";
 import CustomListingFilterPopover from "../components/Listings/CustomListingsFilter";
 import ListingContent from "../components/listingsContent";
 import { transitionVariants } from "../styles/motion-definitions";
@@ -12,7 +14,11 @@ export default function Listings() {
   const [bathrooms, setBathrooms] = useState<string>("all");
   const [petsAllowed, setPetsAllowed] = useState<string>("all");
   const [distanceToUCF, setDistanceToUCF] = useState<number>(100000);
-
+  const router = useRouter();
+  const handleCreateListingClick = () => {
+    // eslint-disable-next-line @typescript-eslint/no-floating-promises
+    router.push("/createListing");
+  };
   return (
     <div className="">
       {
@@ -30,6 +36,9 @@ export default function Listings() {
                 "relative mx-auto flex h-full  max-w-7xl items-center justify-end bg-white pr-8 pt-5"
               }
             >
+              <Button className="mx-4" onClick={handleCreateListingClick}>
+                Create listing
+              </Button>
               <CustomListingFilterPopover
                 price={price}
                 setPrice={setPrice}

a3c23021f3fcdf47847eb0c4c7f27b1a8fbb779c
Sleepingpirates <faizsyed.a@gmail.com>
2023-03-23 13:27:02 -0400

    fix: Refactor authentication redirection process in web app
    - Import `useRouter` from `next/router` in `AuthRedirectWrapper.tsx`
    - Add conditional check in `AuthRedirectWrapper.tsx` to prevent calling `UseAuthRedirect()` on non-root pages
    - Updated several other files with minor changes or bug fixes

--- web/src/components/AuthRedirectWrapper.tsx
+++ web/src/components/AuthRedirectWrapper.tsx
@@ -1,13 +1,15 @@
+import { useRouter } from "next/router";
 import { ReactNode } from "react";
 
 import UseAuthRedirect from "../hooks/useAuthRedirect";
-
 interface Props {
   children: ReactNode;
 }
 
 export default function AuthRedirectWrapper({ children }: Props) {
-  UseAuthRedirect();
-
+  const router = useRouter();
+  if (router.pathname !== "/") {
+    UseAuthRedirect();
+  }
   return <>{children}</>;
 }

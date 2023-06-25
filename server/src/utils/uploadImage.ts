34ef16478b356bee809117db64742fc5c4538bca
joseption <40270262+joseption@users.noreply.github.com>
2023-06-25 17:14:27 -0400

    remove aws s3 upload

--- server/src/utils/uploadImage.ts
+++ server/src/utils/uploadImage.ts
@@ -7,25 +7,25 @@ import { v4 } from 'uuid';
 
 // @params file: string (base64) image file
 export const uploadImage = async (image: string) => {
-  // try {
-  //   const s3 = new S3({
-  //     accessKeyId: process.env.AWSACCESS_KEY,
-  //     secretAccessKey: process.env.AWSSECRET_KEY,
-  //   });
-  //   const base64Data = Buffer.from(image.replace(/^data:image\/\w+;base64,/, ''), 'base64');
-  //   const type = image.split(';')[0].split('/')[1];
+  try {
+    // const s3 = new S3({
+    //   accessKeyId: process.env.AWSACCESS_KEY,
+    //   secretAccessKey: process.env.AWSSECRET_KEY,
+    // });
+    const base64Data = Buffer.from(image.replace(/^data:image\/\w+;base64,/, ''), 'base64');
+    const type = image.split(';')[0].split('/')[1];
 
-  //   const params = {
-  //     Bucket: process.env.AWSBucket,
-  //     Key: `${v4()}.${type}`,
-  //     Body: base64Data,
-  //     ContentEncoding: 'base64',
-  //     ContentType: `image/${type}`,
-  //   };
+    const params = {
+      Bucket: process.env.AWSBucket,
+      Key: `${v4()}.${type}`,
+      Body: base64Data,
+      ContentEncoding: 'base64',
+      ContentType: `image/${type}`,
+    };
 
-  //   const { Location } = await s3.upload(params).promise();
-  //   return Location;
-  // } catch (error) {
-  //   return null;
-  // }
+    //const { Location } = await s3.upload(params).promise();
+    return "";//Location;
+  } catch (error) {
+    return null;
+  }
 };
